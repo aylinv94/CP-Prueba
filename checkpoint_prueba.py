6a72353381775152be3b739804819d2a1cd8124a
PaulaVillarS <paulaa.villar@gmail.com>
2022-09-22 13:01:18 -0300

    checkpoint commit

--- checkpoint_prueba.py
+++ checkpoint_prueba.py
@@ -1,3 +1,13 @@
+import pandas as pd
+import numpy as np
+
 def hola_mundo():
-    #Crear función que muestre el texto hola mundo.
-    return print('hola mundo!!')
+    '''Crear función que muestre el texto hola mundo.'''
+    #Tu código acá
+    a = 'hola mundo, este es el CP de prueba'
+    return print(a)
+
+#def importar():
+ #   """Importe el archivo el archivo Emisiones_CO2.csv"""
+  #  df = pd.read_csv('datasets\Emisiones_CO2.csv')
+   # return print(df.head(1))
